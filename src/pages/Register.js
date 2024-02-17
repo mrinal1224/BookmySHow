ebba5060af9ee07237271cd796201111779a9230
mrinal1224 <iammrinalkc@gmail.com>
2024-02-17 22:32:36 +0530

    push

---
+++ src/pages/Register.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Button, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+
+function Register() {
+  return (
+    <>
+      <header className="App-header">
+        <main className="main-area mw-500 text-center px-3">
+          <section className="left-section">
+            <h1>Register to BookMyShow</h1>
+          </section>
+
+          <section className="right-section">
+            <Form layout="vertical">
+              <Form.Item
+                label="Name"
+                htmlFor="name"
+                name="name"
+                className="d-block"
+                rules={[{ required: true, message: "Name is required" }]}
+              >
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="Enter your name"
+                  rules={[{ required: true, message: "Name is required!" }]}
+                ></Input>
+              </Form.Item>
+
+              <Form.Item
+                label="Email"
+                htmlFor="email"
+                name="email"
+                className="d-block"
+                rules={[{ required: true, message: "Email is required" }]}
+              >
+                <Input
+                  id="email"
+                  type="text"
+                  placeholder="Enter your Email"
+                  rules={[{ required: true, message: "Email is required!" }]}
+                ></Input>
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                htmlFor="password"
+                name="password"
+                className="d-block"
+                rules={[{ required: true, message: "Password is required" }]}
+              >
+                <Input
+                  id="password"
+                  type="text"
+                  placeholder="Enter your Password"
+                  rules={[{ required: true, message: "Password is required!" }]}
+                ></Input>
+              </Form.Item>
+
+              <Form.Item className="d-block">
+                <Button
+                  type="primary"
+                  block
+                  htmlType="submit"
+                  style={{ fontSize: "1rem", fontWeight: "600" }}
+                >
+                  Register
+                </Button>
+              </Form.Item>
+            </Form>
+            <div>
+              <p>
+                Already a user? <Link to="/login">Login now</Link>
+              </p>
+            </div>
+          </section>
+        </main>
+      </header>
+    </>
+  );
+}
+
+export default Register;
